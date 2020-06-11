e5dc203b0b7dd412ca0591eb34adac82986f9c72
Raghavasimhan Sankaranarayanan <aavu@gatech.edu>
2020-06-11 14:39:03 -0700

    Fixed bug in Test_Something.cpp

--- src/Tests/Tests/Test_Something.cpp
+++ src/Tests/Tests/Test_Something.cpp
@@ -1,8 +1,16 @@
-#ifdef WITH_TESTS
-#include "unittest++.h"
+#include "UnitTest++.h"
+//#include <iostream>
 
 SUITE(Something) {
+    TEST(hello)
+    {
+//        std::cout << "hello\n";
+        CHECK_EQUAL(1, 1);
+    }
 
+    TEST(hello2)
+    {
+//        std::cout << "hello2\n";
+        CHECK_EQUAL(1, 0);
+    }
 }
-
-#endif //WITH_TESTS
